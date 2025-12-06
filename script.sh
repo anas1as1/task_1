025727c7445ca489983382e02627067339d8bde6
anas1as1 <nastyapiv2018@gmail.com>
2025-12-06 05:56:23 +0300

    Добавлены функции show_users, show processes, show_help, check_access

--- script.sh
+++ script.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+show_users() {
+    cut -d: -f1,6 /etc/passwd | sort
+}
+
+show_processes() {
+    ps -eo pid,cmd --sort=pid
+}
+
+show_help() {
+    echo "Использование: $0 [OPTIONS]"
+    echo "  -u, --users       показать пользователей"
+    echo "  -p, --processes   показать процессы"
+    echo "  -l, --log FILE    выводить результат в файл"
+    echo "  -e, --errors FILE выводить ошибки в файл"
+    echo "  -h, --help        показать справку"
+    exit 0
+}
+
+check_access() {
+    # Проверка возможности записи в указанный файл
+    touch "$1" 2>/dev/null
+    if [[ $? -ne 0 ]]; then
+        echo "Ошибка: нет доступа к файлу '$1'" >&2
+        exit 1
+    fi
+}
