3122316c61cd5d3c9d48e09f7ac0135134a27186
anas1as1 <nastyapiv2018@gmail.com>
2025-12-06 06:28:04 +0300

    Добавлены глобальные переменные LOG_FILE, ERR_FILE

--- script.sh
+++ script.sh
@@ -26,3 +26,26 @@ check_access() {
         exit 1
     fi
 }
+
+  GNU nano 8.7                       script.sh                        Modified
+show_help() {
+    echo "Использование: $0 [OPTIONS]"
+    echo "  -u, --users       показать пользователей"
+    echo "  -p, --processes   показать процессы"
+    echo "  -l, --log FILE    выводить результат в файл"
+    echo "  -e, --errors FILE выводить ошибки в файл"
+    echo "  -h, --help        показать справку"
+    exit 0
+}
+
+check_access() {
+    # Проверка возможности записи в указанный файл
+    touch "$1" 2>/dev/null
+    if [[ $? -ne 0 ]]; then
+        echo "Ошибка: нет доступа к файлу '$1'" >&2
+        exit 1
+    fi
+}
+
+LOG_FILE=""
+ERR_FILE=""
